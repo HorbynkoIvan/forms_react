e6304ca7aeb15202290e5191570f1d70982c05c0
Horbynko Ivan <horbynko.i@gmail.com>
2019-02-22 18:23:48 +0200

    formic form v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import BasicFormik from './formik/basicForm';
+import './scss/common.scss';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        test
+        <BasicFormik/>
       </div>
     );
   }
