3d9543a62a7b5521d5161054e10864509615cda5
Horbynko Ivan <horbynko.i@gmail.com>
2019-02-24 14:03:52 +0200

    added yup validation for formik

--- src/formik/basicForm3_with_yup.js
+++ src/formik/basicForm3_with_yup.js
@@ -1,29 +1,59 @@
 import React from 'react';
-import {Formik, Form, Field} from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+
 import './basicForm.scss'
 
 //Formik has a special config option / prop for Yup called validationSchema which will automatically
 // transform Yup's validation errors into a pretty object whose keys match values and touched
 
-const ValidationSchemaExample = () => {
-    return (
-        <div className='basicFormik'>
-            <h1>Signin</h1>
-            <Formik>
-                {
-                    ({}) => (
-                        <Form>
-                            <Field name='firstName'/>
-                            <Field name='lastName'/>
-                            <Field type='email' name='email'/>
-                            <button>Submit</button>
-                        </Form>
-                    )
-                }
-            </Formik>
-        </div>
-    )
-}
+
+const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    lastName: Yup.string()
+        .min(5, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    email: Yup.string()
+        .email('Invalid email')
+        .required('Required'),
+});
+
+export const ValidationSchemaExample = () => (
+    <div className='basicFormik'>
+        <h1>Signup</h1>
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+                // same shape as initial values
+                console.log(values);
+            }}
+        >
+            {({ errors, touched }) => (
+                <Form>
+                    <Field name="firstName" />
+                    {errors.firstName && touched.firstName ? (
+                        <div>{errors.firstName}</div>
+                    ) : null}
+                    <Field name="lastName" />
+                    {errors.lastName && touched.lastName ? (
+                        <div>{errors.lastName}</div>
+                    ) : null}
+                    <Field name="email" type="email" />
+                    {errors.email && touched.email ? <div>{errors.email}</div> : null}
+                    <button type="submit">Submit</button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+);
 
 export default ValidationSchemaExample;
